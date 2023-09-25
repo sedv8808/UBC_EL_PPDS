6446a89efbead66172469e3c6a1b32c1c35f7e89
Socorro Dominguez <s.dominguez@ht-data.com>
2023-09-25 09:45:11 -0700

    M4

---
+++ word_count.py
@@ -0,0 +1,13 @@
+def word_count(str):
+    counts = dict()
+    words = str.split()
+
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    counts = dict(sorted(counts.items(), key=lambda item: item[1],reverse=True))
+
+    return counts
